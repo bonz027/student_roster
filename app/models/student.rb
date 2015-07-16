851304dea15719c24e097d9001f8e6872873b148
Omar Castro <ocastro24@yahoo.com>
2015-07-16 13:06:08 -0600

    Added Validations
    [#99155252]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ActiveRecord::Base
+
+  validates_each :first_name, :last_name do |record, attr, value|
+    record.errors.add(attr, 'Name must start with a upper case') if value =~
+      /\A[[:lower:]]/
+  end
+  validates :email, presence: true
+
+  validates :cell_phone, :home_phone, :work_phone, :presence => true,
+                                                   :numericality => true,
+                                                   :length => { :minimum => 10 }
 end
+
